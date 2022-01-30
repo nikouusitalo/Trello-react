8e690ff90bdbcbcf81e7afd46294f17257260e91
Niko Uusitalo <niko.u@hotmail.fi>
2022-01-30 16:04:06 +0200

    Adding style

---
+++ trello-react/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import { AppContainer } from './styles';
+
+
+
+
+
+export const App = () => {
+  return (
+    <AppContainer>
+    Columns will go here
+</AppContainer>
+  )
+}
+
