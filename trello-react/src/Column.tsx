8e6fce24361f2f2d30ffbbbbf9bb7fd6d1b42364
Niko Uusitalo <niko.u@hotmail.fi>
2022-02-01 00:35:05 +0200

    card, and column component

---
+++ trello-react/src/Column.tsx
@@ -0,0 +1,19 @@
+import {FC} from "react"
+import { ColumnContainer, ColumnTitle } from "./styles"
+
+
+type ColumnProps = {
+    text: string
+    children?: React.ReactNode
+    }
+  
+
+
+export const Column: FC<ColumnProps> = ({ text, children }) => {
+    return (
+    <ColumnContainer>
+    <ColumnTitle>{text}</ColumnTitle>
+    {children}
+    </ColumnContainer>
+    )
+    }
